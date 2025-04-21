0f161e0f583f787aa367140a3bb954a399e8204b
Garygrentz <garygrentz@hotmail.com>
2025-04-21 18:10:21 -0500

    Add files via upload
    Primera actualización de mi primer programa

---
+++ Dashboard_Final.py
@@ -0,0 +1,102 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+# Junta datos de establecimietos y ubiego
+dfEstab = pd.read_csv("TB_CENTRO_VACUNACION.csv", delimiter=";")
+#dfEstab = dfEstab.drop(["id_eess"])
+dfUbi = pd.read_csv("TB_UBIGEOS.csv", delimiter=";")
+# Elimina columnas que sobran
+dfUbi = dfUbi.drop(["ubigeo_reniec","ubigeo_inei","departamento_inei","provincia_inei","region",
+            "macroregion_inei","macroregion_minsa","iso_3166_2","fips","superficie","Frontera",
+            "latitud","longitud"], axis=1)
+# Junta dataframes
+dfCentros = pd.merge(dfEstab, dfUbi, on="id_ubigeo")
+dfCentros = dfCentros.drop(["id_centro_vacunacion","id_ubigeo","id_eess"], axis=1)
+
+# Elimina filas que tiene Latitud o Longitud en cero
+dfCentros.drop(dfCentros[(dfCentros["latitud"]==0) & (dfCentros["longitud"]==0)].index, inplace=True )
+
+st.title("Centros de Vacunación")
+menu = ["Todos","Ubicación","Personalizado (Distritos)","Centro de Vacunación"]
+
+choice = st.sidebar.selectbox("Menu",menu)
+color = st.sidebar.color_picker("Color", value="#9E1FA2")
+
+config = {'scrollZoom': True}
+
+if choice=="Todos":
+    # Ordena dataframe
+    dfCentros.sort_values(["departamento","provincia","distrito","nombre"],inplace=True)
+
+    with st.expander("Ver datos"):
+        dfCentros.reset_index(inplace=True, drop=True)
+        st.dataframe(dfCentros)
+
+    fig = px.scatter_mapbox(dfCentros, lat="latitud", lon="longitud", hover_name="nombre", hover_data=["departamento","provincia","distrito"],
+                             color_discrete_sequence=[color], zoom=4, height=700)
+    fig.update_layout(mapbox_style="open-street-map")
+    st.plotly_chart(fig, config=config)
+
+elif choice=="Ubicación":
+    #pass
+    # Ordena dataframe
+    dfCentros.sort_values(["departamento","provincia","distrito","nombre"],inplace=True)
+
+    dep_list = dfCentros["departamento"].unique().tolist()
+    selected_dep = st.sidebar.selectbox("Departamento", dep_list)
+
+    df_tmp = dfCentros[(dfCentros["departamento"]==selected_dep)]
+    prov_list = df_tmp["provincia"].unique().tolist()
+    selected_prov = st.sidebar.selectbox("Provincia", prov_list)    
+
+    df_tmp = dfCentros[(dfCentros["departamento"]==selected_dep) & (dfCentros["provincia"]==selected_prov) ]
+    dist_list = df_tmp["distrito"].unique().tolist()
+    selected_dist = st.sidebar.selectbox("Distrito", dist_list)
+
+    with st.expander("Ver datos"):
+        df = dfCentros[(dfCentros["departamento"]==selected_dep) & (dfCentros["provincia"]==selected_prov) & (dfCentros["distrito"]==selected_dist) ]
+        df.reset_index(inplace=True, drop=True)
+        st.dataframe(df)
+
+    fig = px.scatter_mapbox(df, lat="latitud", lon="longitud", hover_name="nombre", hover_data=["departamento","provincia","distrito"],
+                             color_discrete_sequence=[color], zoom=7, height=700)
+    fig.update_layout(mapbox_style="open-street-map")
+    st.plotly_chart(fig, config=config)
+
+elif choice=="Personalizado (Distritos)":
+    # Ordena dataframe
+    dfCentros.sort_values(["distrito","nombre"],inplace=True)
+
+    seldist_list = dfCentros["distrito"].unique().tolist()
+    selected_dist = st.multiselect("Distritos",seldist_list, default=["LIMA"])
+    with st.expander("Ver datos"):
+        gdf = dfCentros[dfCentros["distrito"].isin(selected_dist)]
+        gdf.reset_index(inplace=True, drop=True)
+        st.dataframe(gdf)
+
+    fig = px.scatter_mapbox(gdf, lat="latitud", lon="longitud", hover_name="nombre", hover_data=["departamento","provincia","distrito"],
+                             color_discrete_sequence=[color], zoom=6, height=700)
+    fig.update_layout(mapbox_style="open-street-map")
+    st.plotly_chart(fig, config=config)
+
+elif choice=="Centro de Vacunación":
+    # Ordena dataframe
+    dfCentros.sort_values(["nombre"],inplace=True)
+
+    nom_list = dfCentros["nombre"].unique().tolist()
+    selected_nombre = st.multiselect("Centro de Vacunación",nom_list, default=["CENTRO DE SALUD UNIDAD VECINAL Nº 3"])
+    with st.expander("Ver datos"):
+        gdf = dfCentros[dfCentros["nombre"].isin(selected_nombre)]
+        gdf.reset_index(inplace=True, drop=True)
+        st.dataframe(gdf)
+
+    fig = px.scatter_mapbox(gdf, lat="latitud", lon="longitud", hover_name="nombre", hover_data=["departamento","provincia","distrito"],
+                             color_discrete_sequence=[color], zoom=4, height=700)
+    fig.update_layout(mapbox_style="open-street-map")
+    st.plotly_chart(fig, config=config)
+
+else:
+    st.subheader("About")
+
